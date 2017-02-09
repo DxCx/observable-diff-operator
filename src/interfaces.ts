e96830fee46cafe10afecc555cf721a084e86d7a
Hagai Cohen <hagai.co@gmail.com>
2017-02-09 16:02:21 +0200

    chore(interface): fix Observer interface

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,9 @@
 export type IObservableDiffType = 'init' | 'update' | 'error' | 'complete';
 
 export interface Observer<T> {
-  next?: (value: T) => void;
-  error?: (error: Error) => void;
-  complete?: () => void;
+  next: (value: T) => void;
+  error: (error: Error) => void;
+  complete: () => void;
 }
 
 export interface IObservableDiff {
